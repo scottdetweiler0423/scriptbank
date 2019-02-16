b4fc82b37eef8e305832159392aae65e7d161474
scottdetweiler0423 <47368387+scottdetweiler0423@users.noreply.github.com>
2019-02-15 21:42:48 -0500

    Add files via upload

---
+++ CustomerSentimentAnalysis.py
@@ -0,0 +1,35 @@
+from textblob import TextBlob as tb
+import pandas as pd
+import csv
+
+output_csv = 'NissanReviewSentiments.csv'
+input_csv = 'NissanCustomerReviews.csv'
+
+file_reader =  open(input_csv, "r", encoding = 'utf-8')
+read = csv.reader(file_reader)
+reviews = [x[0] for x in read if x]
+reviewlist = []
+
+for review in reviews:
+    reviewlist.append(review)
+
+
+review_dict = {'review': [],
+               'sentiment': []
+                }
+
+for review in reviewlist:
+    # url_start_time = time.time() #start clock
+    t_review = tb(review)
+    review_sentiment = t_review.sentiment.polarity
+
+    review_dict['review'].append(review)
+    review_dict['sentiment'].append(review_sentiment)
+
+# writing dealer_dict to csv
+df = pd.DataFrame(review_dict)
+print(df.head())
+with open(output_csv, 'a', newline='', encoding = "utf-8") as f:
+    df.to_csv(f, header=False)
+
+
